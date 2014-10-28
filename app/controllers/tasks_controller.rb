e0d9338cd4424b10730e221389da0a162e972c5c
Kang Kyu Lee <kangkyu1111@gmail.com>
2014-10-27 18:22:23 -0700

    add ajax ujs for new create destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   def index
+    sleep 2
     @complete_tasks = Task.where(complete: true)
     @incomplete_tasks = Task.where(complete: false)
   end
@@ -7,13 +8,19 @@ class TasksController < ApplicationController
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to tasks_url
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
   end
 
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
-    redirect_to tasks_url
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
   end
 
   def new
@@ -23,7 +30,10 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to tasks_url
+    respond_to do |format|
+      format.html { redirect_to tasks_url }
+      format.js
+    end
   end
 
   private
